3d809aceb672045a88266e64025d09be1b168317
DaviDChoudharY <122169398+DaviDChoudharY@users.noreply.github.com>
2025-04-09 04:56:12 +0530

    Update with env setup

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import "../CSS/Profile.css";
+import "../css/Profile.css";
 
 const Profile = () => {
   const [userData, setUserData] = useState(null);
